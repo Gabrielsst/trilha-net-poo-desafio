7bc152fc1ca94e20d3d23f508e74ba6ac7fa0bb4
Gabriel Santos Teixeira <69646007+Gabrielsst@users.noreply.github.com>
2023-09-28 16:42:32 -0300

    Modificando todos os arquivos

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+System.Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("123456", "Modelo 1", "111111111", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+System.Console.WriteLine();
+
+System.Console.WriteLine("Smartphone iPhone:");
+Smartphone iphone = new Iphone("4987", "Modelo 2", "222222222", 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
